07f2795b7ae18c8c32cd4f45e9b8d00410e3fe6a
Nikita Pozdeev <octoberallover.nn@gmail.com>
2021-07-23 22:03:10 +0400

    replace cra example code with real one

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+function PermitBackupUIApp() {
+  return (
+    <div>
+      <header>Permit Backup v.{process.env.REACT_APP_VERSION}</header>
+      <main>
+        content here
+      </main>
+      <footer>
+        footer here
+      </footer>
+    </div>
+  );
+}
+
+export default PermitBackupUIApp;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import PermitBackupUIApp from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+const APP_ROOT_ID = 'permit-backup-ui-root';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PermitBackupUIApp />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById(APP_ROOT_ID)
 );
 
 // If you want to start measuring performance in your app, pass a function
