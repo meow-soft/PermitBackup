c7e94cc411303d7dc92981d57f227b7759a12976
Nikita Pozdeev <octoberallover.nn@gmail.com>
2021-07-27 23:01:06 +0400

    add user, page components

---
+++ src/components/Page/Page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page = () => {
+  return <div className="main">main</div>;
+};
+
+export default Page;
---
+++ src/components/User/User.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './User.css';
+
+const User = () => {
+  return (
+    <a>
+      <img
+        src="https://i.pravatar.cc/300"
+        alt="user-avatar"
+        className="navAvatar"
+      />
+    </a>
+  );
+};
+
+export default User;
