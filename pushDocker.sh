8a3526459e11b02de82fb5c71a1bf2fad1ed3156
ybogdanov <79097146710@yandex.ru>
2022-10-05 22:10:36 +0400

    sh file fix

--- pushDocker.sh
+++ pushDocker.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-GIT_HASH= git rev-parse --short HEAD
-echo "$GIT_HASH"
-gradle clean bootJar -Pvaadin.productionMode
-#docker build --tag wonderkot/backuper:$GIT_HASH .
-#docker push wonderkot/backuper:$GIT_HASH
+GIT_HASH="$(git rev-parse --short HEAD)"
+
+docker build --tag wonderkot/backuper:$GIT_HASH .
+docker push wonderkot/backuper:$GIT_HASH
 
 docker build --tag wonderkot/backuper:latest .
 docker push wonderkot/backuper:latest
